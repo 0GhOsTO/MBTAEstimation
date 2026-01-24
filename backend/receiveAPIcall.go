fd9e82fa1a8f984cc7d817baf059bd46a83c225c
0GhOsTO <acho7@bu.edu>
2026-01-24 16:04:08 -0500

    API every 30 seconds updated

--- backend/receiveAPIcall.go
+++ backend/receiveAPIcall.go
@@ -10,15 +10,20 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 )
 
 func fetchPrediction(stopID string) {
+
+	// grab the API key.
 	key := os.Getenv("MBTA_API_KEY")
 	if key == "" {
 		panic("MBTA_API_KEY not set")
 	}
+
+	// constructing the request.
 	url := fmt.Sprintf("https://api-v3.mbta.com/predictions?filter[stop]=%s", stopID)
 	req, err := http.NewRequestWithContext(context.Background(), "GET", url, nil)
 	if err != nil {
@@ -26,15 +31,18 @@ func fetchPrediction(stopID string) {
 	}
 	req.Header.Set("x-api-key", key)
 	client := &http.Client{}
+
 	resp, err := client.Do(req)
 	if err != nil {
 		panic(err)
 	}
+
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		panic(err)
 	}
+
 	fmt.Println(string(body))
 }
 
@@ -43,5 +51,25 @@ func main() {
 	if err != nil {
 		fmt.Println("No .env found")
 	}
-	fetchPrediction("70020") // example stop ID
+
+	// B line stop IDs:
+	/* 70111 70113 70115 70117 70121 70125 70127 70129
+	70131 70135 70137 70139 70141 70143 70145 70147 70149 70196 71151
+	*/
+
+	// 1. Every 30 seconds, fetch prediction for a specific stop ID.
+	// 2. Store the fetched data until the vehicle arrives.
+	// 3. Once the vehicle arrived, grade the prediction accuracy.
+
+	// 1. Every 30 seconds ...
+	ticker := time.NewTicker(30 * time.Second)
+	defer ticker.Stop()
+	// call once right away
+	for {
+		<-ticker.C
+		fetchPrediction("70020") // example stop ID
+	}
 }
+
+//Mathematical equations
+//error = | predicted_arrival_time - actual_arrival_time |
